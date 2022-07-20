462387b4a562856a22ef5bdc56d8b5f78eb885fa
Foram Patel <fragrance9884@gmail.com>
2022-07-20 21:25:29 +0100

    change value of B

--- javaoperators.java
+++ javaoperators.java
@@ -1,7 +1,7 @@
 public class javaoperators {
     public static void main(String[] args) {
         int A=20;
-        int B=40;
+        int B=60;
         System.out.println("A="+A);
         System.out.println("B="+B);
         System.out.println(A+B);
