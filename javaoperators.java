93de5f50359029bdc135acb01ac3038dac77f7ad
Foram Patel <fragrance9884@gmail.com>
2022-07-20 15:48:31 +0100

    my commit

---
+++ javaoperators.java
@@ -0,0 +1,10 @@
+public class javaoperators {
+    public static void main(String[] args) {
+        int A=20;
+        int B=40;
+        System.out.println("A="+A);
+        System.out.println("B="+B);
+        System.out.println(A+B);
+        System.out.println(A-B);
+    }
+}
