b46b560894d1de723a0315fbe4ea4e7aad08adae
Foram Patel <fragrance9884@gmail.com>
2022-07-21 18:57:13 +0100

    integer Datatype

--- javaoperators.java
+++ javaoperators.java
@@ -2,6 +2,7 @@ public class javaoperators {
     public static void main(String[] args) {
         int A=20;
         int B=60;
+        System.out.println("Integer Datatype");
         System.out.println("A="+A);
         System.out.println("B="+B);
         System.out.println(A+B);
